c02bf88b1140bc409fa1ce0f62a060f7b9668373
ggingmin <ggingmin@gmail.com>
2021-11-30 20:16:32 +0900

    Errors resolved

--- cards/src/main/java/com/ggingmin/cards/CardsApplication.java
+++ cards/src/main/java/com/ggingmin/cards/CardsApplication.java
@@ -2,8 +2,17 @@ package com.ggingmin.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
+@ComponentScans({ @ComponentScan("com.ggingmin.cards.controller") })
+@EnableJpaRepositories("com.ggingmin.cards.repository")
+@EntityScan("com.ggingmin.cards.model")
 public class CardsApplication {
 
 	public static void main(String[] args) {
--- cards/src/main/java/com/ggingmin/cards/controller/CardsController.java
+++ cards/src/main/java/com/ggingmin/cards/controller/CardsController.java
@@ -1,9 +1,15 @@
 package com.ggingmin.cards.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.ggingmin.cards.config.CardsServiceConfig;
 import com.ggingmin.cards.model.Card;
 import com.ggingmin.cards.model.Customer;
+import com.ggingmin.cards.model.Properties;
 import com.ggingmin.cards.repository.CardsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +22,9 @@ public class CardsController {
     @Autowired
     private CardsRepository cardsRepository;
 
+    @Autowired
+    private CardsServiceConfig cardsConfig;
+
     @PostMapping("/myCards")
     public List<Card> getCardDetails(@RequestBody Customer customer) {
         List<Card> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
@@ -25,4 +34,14 @@ public class CardsController {
             return null;
         }
     }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
+    }
+
 }
