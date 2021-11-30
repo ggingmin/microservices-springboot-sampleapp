51d0f024335da35b7e12a38659a80aad2f498c7e
ggingmin <ggingmin@gmail.com>
2021-11-30 19:40:48 +0900

    Accounts Service updated to get properties from Config Server(Errors not resolved)

--- accounts/src/main/java/com/ggingmin/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/ggingmin/accounts/controller/AccountsController.java
@@ -1,18 +1,28 @@
 package com.ggingmin.accounts.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.ggingmin.accounts.config.AccountsServiceConfig;
 import com.ggingmin.accounts.model.Account;
 import com.ggingmin.accounts.model.Customer;
+import com.ggingmin.accounts.model.Properties;
 import com.ggingmin.accounts.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class AccountsController {
 
     @Autowired
     private AccountsRepository accountsRepository;
+
+    @Autowired
+    AccountsServiceConfig accountsConfig;
 
     @PostMapping("/myAccount")
     public Account getAccountDetails(@RequestBody Customer customer) {
@@ -23,5 +33,17 @@ public class AccountsController {
         } else {
             return null;
         }
+    }
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(
+                accountsConfig.getMsg(),
+                accountsConfig.getBuildVersion(),
+                accountsConfig.getMailDetails(),
+                accountsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
